b5e1b27af0b33c784c1da562095c887ebc20980a
eatradish <sakiiily@aosc.io>
2021-11-28 13:38:24 +0800

    lint: fix typo

--- build.rs
+++ build.rs
@@ -14,7 +14,7 @@ fn generate_completions() {
 
 fn main() {
     if cfg!(feature = "aosc") && !OsRelease::new().unwrap().name.contains("AOSC OS") {
-        panic!("If you detect that you are using aosc feature but your system is not AOSC OS, please use --no-default-feature to compile");
+        panic!("If you detect that you are using aosc feature but your system is not AOSC OS, please use --no-default-features to compile");
     } else if !cfg!(feature = "aosc") && OsRelease::new().unwrap().name.contains("AOSC OS") {
         println!("cargo:warning=Detected that you are using AOSC OS but did not turn on feature aosc, if you don't know, use --features aosc to turn on this fearture")
     }
